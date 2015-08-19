743d5a659e24505456c1e828157dcf4a2c8931f1
Misha Ponizil <misha.ponizil@gmail.com>
2015-08-19 16:01:05 -0400

    Comment about webpack config gotcha

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  // Need an object instead of just a string, I think because Karma
+  // makes two builds where one requires the other, leading to:
+  // `Error: a dependency to an entry point is not allowed`
+  // https://github.com/webpack/webpack/issues/300
   entry: {
     'storage-expires': './storage-expires'
   },
