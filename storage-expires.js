22d918f17f4988ac6e6f743125fb39d298e03e75
Misha Ponizil <misha.ponizil@gmail.com>
2015-08-20 11:28:08 -0400

    Move lsWrapper to util file

--- storage-expires.js
+++ storage-expires.js
@@ -5,19 +5,7 @@
 */
 
 var assign = require('./util/assign');
-
-var lsWrapper = {
-  get: function(key) {
-    return localStorage.getItem(key);
-  },
-  set: function(key, value, options) {
-    return localStorage.setItem(key, value);
-  },
-  unset: function(keys) {
-    if (!(keys instanceof Array)) keys = [keys];
-    for (i = 0; i < keys.length; i++) localStorage.removeItem(keys[i]);
-  }
-};
+var lsWrapper = require('./util/ls-wrapper');
 
 // StorageExpires
 // --------------
